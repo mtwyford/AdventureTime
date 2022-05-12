ee16ac9ab4b837c7b911706e402983e847be48e7
pangkyl24 <77285931+pangkyl24@users.noreply.github.com>
2022-05-12 15:09:42 -0400

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,20 @@
+//Must Use Scanner Class
+import java.util.Scanner;
+
+public class Game{
+
+  private String playerName;
+  private String optionsPicked;
+
+  //CONSTRUCTOR
+  public Game(){
+
+  }
+  //This is where the bulk of your code should be
+  public String play(){
+
+  }
+
+  //If needed, create more classes
+  
+}
